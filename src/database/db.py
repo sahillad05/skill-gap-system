a580cc8dfed173b79755ce7be4ea07f3dc8eb23e
Sahil Lad <141474253+sahillad05@users.noreply.github.com>
2026-04-14 00:36:49 +0530

    Added GitHub Actions automation and migrated database to Supabase

--- src/database/db.py
+++ src/database/db.py
@@ -6,4 +6,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
